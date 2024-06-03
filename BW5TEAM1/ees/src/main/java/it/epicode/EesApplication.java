fedb5e04a95185618b15345ac58bca1655e27309
Francesca <52604627+FrancescaMuia@users.noreply.github.com>
2024-06-03 10:13:04 +0200

    Add files via upload

---
+++ BW5TEAM1/ees/src/main/java/it/epicode/EesApplication.java
@@ -0,0 +1,13 @@
+package it.epicode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EesApplication.class, args);
+	}
+
+}
